c9e53e48ca23f2a6f59e7f1d442e12ff616d583a
Jake Gordin <gcg0038@auburn.edu>
2025-02-15 17:34:31 -0600

    Initial Commit

---
+++ Mobile1_A7_Presidents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile1_A7_Presidents
+//
+//  Created by Jake Gordin on 2/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
